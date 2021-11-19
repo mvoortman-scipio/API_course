ce616e2cf6b9f24d172027fb0c4ef39cd4273c9f
Michiel Voortman <Michiel.Voortman@users.noreply.github.com>
2021-11-19 09:48:52 +0100

    uodated the uwsgi and app file to fixed the database part

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
+
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
-from datetime import timedelta
 from resources.store import Store, StoreList
+from datetime import timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "Vicento1816!"
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 app.config["JWT_AUTH_URL_RULE"] = "/login"
 jwt = JWT(app, authenticate, identity) #/auth
